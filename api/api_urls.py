3d05be7259930df574444baf15a6f1737dcb5c85
Andrey <andr_htr@mail.ru>
2022-01-12 21:36:21 +0500

    add tokens view

--- api/api_urls.py
+++ api/api_urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from .views import CourseView, CourseRatingCreateView
 
 
@@ -9,5 +10,7 @@ router.register('courses', CourseView, basename='all_courses')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('tokens/', TokenObtainPairView.as_view()),
+    path('tokens/refresh/', TokenRefreshView.as_view()),
     path('course/rating/', CourseRatingCreateView.as_view())
 ]
