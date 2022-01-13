72fd5c04d3f7a71614fd986e2f430a70b9865142
Andrey <andr_htr@mail.ru>
2022-01-13 12:53:20 +0500

    delete unused imports

--- api/views.py
+++ api/views.py
@@ -1,6 +1,6 @@
 from rest_framework.exceptions import ValidationError
 from rest_framework.viewsets import ReadOnlyModelViewSet
-from rest_framework.generics import CreateAPIView, ListAPIView
+from rest_framework.generics import CreateAPIView
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from .models import Content, Course, Rating
